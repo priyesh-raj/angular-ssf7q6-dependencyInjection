b8f70fc5c726ab21cfb7b8ed0ad2131c3e65cc73
priyesh-raj <priyesh.raj9@gmail.com>
2020-04-30 18:52:11 +0530

    3 files modified, 5 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  activeUser:string[] = ['Max','Charles']
+  inactiveUser: string[] = ['Alonso','Michael']
 }
---
+++ src/app/inactiveuser/inactiveuser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inactiveuser',
+  templateUrl: './inactiveuser.component.html',
+  styleUrls: ['./inactiveuser.component.css']
+})
+export class InactiveuserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
